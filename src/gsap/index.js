4a46f1668db41ab648e0774d4816191a0c6f818c
giokakee <75224359+giokakee@users.noreply.github.com>
2022-09-14 12:48:31 +0400

    changed ab values styling

--- src/gsap/index.js
+++ src/gsap/index.js
@@ -33,6 +33,7 @@ let sectionAnimation = () => {
         opacity: 0,
         delay: config.aboutUs.delay,
       })
+      .from(".abVal", { duration: 1.5, stagger: 0.3, y: 500, opacity: 0 })
       .from(
         ".aboutUs-title1",
         {
@@ -44,7 +45,6 @@ let sectionAnimation = () => {
         "-=0.1"
       )
       .from(".aboutUs-right__bracket", { duration: 1, opacity: 0 }, "-=0.1")
-      .from(".abVal", { duration: 1.5, stagger: 0.3, y: 500, opacity: 0 })
       .from(".bug", { duration: 1.5, x: -500, opacity: 0 })
       .from(
         ".aboutUs-arrow",
